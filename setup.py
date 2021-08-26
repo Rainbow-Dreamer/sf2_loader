19021d0a57592dc7a45b0e4149e74def98b111cb
Rainbow Dreamer <2180502841@qq.com>
2021-08-26 13:20:07 +0800

    update

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
 from setuptools import setup, find_packages
 from os import path
 
-setup(
-    name='matrixpro',
-    packages = ['matrixpro'],
-    version='0.27',
-    license='AGPLv3',
-    description=
-    'This is a python module for handling matrices, including matrix calculation, analysis and algorithms.',
-    author='Rainbow-Dreamer',
-    author_email='1036889495@qq.com',
-    url='https://github.com/Rainbow-Dreamer/matrixpro',
-    download_url=
-    'https://github.com/Rainbow-Dreamer/matrixpro/archive/0.27.tar.gz',
-    keywords=['matrix', 'mathematics', 'statistics'],
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'Topic :: Software Development :: Build Tools',
-        'License :: OSI Approved :: GNU Affero General Public License v3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
-    ],
-    long_description=open('README.md', encoding='utf-8').read(),
-    long_description_content_type='text/markdown',
-    include_package_data=True)
+setup(name='sf2_loader',
+      packages=find_packages(),
+      version='0.11',
+      license='GPLv3',
+      description=
+      'This is an easy-to-use soundfonts loader and audio renderer in python',
+      author='Rainbow-Dreamer',
+      author_email='1036889495@qq.com',
+      url='https://github.com/Rainbow-Dreamer/sf2_loader',
+      download_url=
+      'https://github.com/Rainbow-Dreamer/sf2_loader/archive/0.11.tar.gz',
+      keywords=['soundfont', 'sf2', 'python'],
+      install_requires=['pydub', 'simpleaudio', 'musicpy'],
+      classifiers=[
+          'Development Status :: 3 - Alpha',
+          'Intended Audience :: Developers',
+          'Topic :: Software Development :: Build Tools',
+          'License :: OSI Approved :: GNU Affero General Public License v3',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
+          'Programming Language :: Python :: 3.9',
+          'Programming Language :: Python :: 3.10',
+      ],
+      long_description=open('README.md', encoding='utf-8').read(),
+      long_description_content_type='text/markdown',
+      include_package_data=True)
