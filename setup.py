13761329832208212d3c484abf76f2239c8d8348
Rainbow Dreamer <2180502841@qq.com>
2021-09-07 02:02:20 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from os import path
 
 setup(name='sf2_loader',
       packages=find_packages(),
-      version='0.33',
+      version='0.34',
       license='GPLv3',
       description=
       'This is an easy-to-use soundfonts loader, player and audio renderer in python',
@@ -11,7 +11,7 @@ setup(name='sf2_loader',
       author_email='1036889495@qq.com',
       url='https://github.com/Rainbow-Dreamer/sf2_loader',
       download_url=
-      'https://github.com/Rainbow-Dreamer/sf2_loader/archive/0.33.tar.gz',
+      'https://github.com/Rainbow-Dreamer/sf2_loader/archive/0.34.tar.gz',
       keywords=['soundfont', 'sf2', 'python'],
       install_requires=['pydub', 'simpleaudio', 'musicpy', 'numpy'],
       classifiers=[
