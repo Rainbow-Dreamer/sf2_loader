fb6dcc6e50fa91e922de06b387abb29bc8c81520
Rainbow <2180502841@qq.com>
2021-11-29 04:04:10 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,31 @@
 from setuptools import setup, find_packages
 from os import path
 
-setup(name='sf2_loader',
-      packages=find_packages(),
-      version='0.78',
-      license='LGPLv2.1',
-      description=
-      'This is an easy-to-use soundfonts loader, player and audio renderer in python',
-      author='Rainbow-Dreamer',
-      author_email='1036889495@qq.com',
-      url='https://github.com/Rainbow-Dreamer/sf2_loader',
-      download_url=
-      'https://github.com/Rainbow-Dreamer/sf2_loader/archive/0.78.tar.gz',
-      keywords=['soundfont', 'sf2', 'python'],
-      install_requires=['pydub', 'musicpy', 'numpy', 'py'],
-      classifiers=[
-          'Development Status :: 3 - Alpha',
-          'Intended Audience :: Developers',
-          'Topic :: Software Development :: Build Tools',
-          'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
-          'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',
-          'Programming Language :: Python :: 3.8',
-          'Programming Language :: Python :: 3.9',
-          'Programming Language :: Python :: 3.10',
-      ],
-      long_description=open('README.md', encoding='utf-8').read(),
-      long_description_content_type='text/markdown',
-      include_package_data=True)
+setup(
+    name='sf2_loader',
+    packages=find_packages(),
+    version='0.78',
+    license='LGPLv2.1',
+    description=
+    'This is an easy-to-use soundfonts loader, player and audio renderer in python',
+    author='Rainbow-Dreamer',
+    author_email='1036889495@qq.com',
+    url='https://github.com/Rainbow-Dreamer/sf2_loader',
+    download_url=
+    'https://github.com/Rainbow-Dreamer/sf2_loader/archive/0.78.tar.gz',
+    keywords=['soundfont', 'sf2', 'python'],
+    install_requires=['pydub', 'musicpy', 'numpy', 'py'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    include_package_data=True)
